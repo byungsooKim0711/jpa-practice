12e56123dfa3976444284b26236007b38fdd6072
kimbs <vlakd0711@naver.com>
2019-03-29 11:45:04 +0900

    webmvcconfig

--- src/main/java/org/kimbs/jpademo/config/WebMvcConfig.java
+++ src/main/java/org/kimbs/jpademo/config/WebMvcConfig.java
@@ -1,21 +1,14 @@
 package org.kimbs.jpademo.config;
 
-import org.kimbs.jpademo.logging.ControllerLogInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    
-    @Bean
-    public ControllerLogInterceptor requestHandler() {
-        return new ControllerLogInterceptor();
-    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(requestHandler());
+        
     }
 }
