52ee83156690d5875e1a3ef1b717f58910effabb
kimbs <vlakd0711@naver.com>
2019-04-01 11:00:17 +0900

    테스트코드 디렉터리 구조 변경

--- src/test/java/org/kimbs/jpademo/SuiteTests.java
+++ src/test/java/org/kimbs/jpademo/SuiteTests.java
@@ -3,10 +3,10 @@ package org.kimbs.jpademo;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.kimbs.jpademo.unittests.UserControllerTests3;
-import org.kimbs.jpademo.unittests.UserRepositoryTests;
-import org.kimbs.jpademo.unittests.UserServiceTests;
-import org.kimbs.jpademo.unittests.UserValidationTests;
+import org.kimbs.jpademo.unittests.user.UserControllerTests3;
+import org.kimbs.jpademo.unittests.user.UserRepositoryTests;
+import org.kimbs.jpademo.unittests.user.UserServiceTests;
+import org.kimbs.jpademo.unittests.user.UserValidationTests;
 
 @RunWith(Suite.class)
 @SuiteClasses(
